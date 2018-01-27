0a93ab2f5430d36d9590c1de92bcf2335eec7316
David Luecke <daff@neyeon.com>
2018-01-26 16:40:31 -0800

    Setup for using Express middleware with authenticaiton

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,34 @@
-module.exports = function (app) { // eslint-disable-line no-unused-vars
-  // Add your custom middleware here. Remember, that
-  // in Express the order matters
+const cookieParser = require('cookie-parser');
+const { authenticate } = require('@feathersjs/authentication').express;
+
+module.exports = function (app) {
+  // Use EJS as the view engine (using the `views` folder by default)
+  app.set('view engine', 'ejs');
+
+  // Render the /login view
+  app.get('/login', (req, res) => res.render('login'));
+
+  // Render the protected chat page
+  app.get('/chat', cookieParser(), authenticate('jwt'), async (req, res) => {
+    // `req.user` is set by `authenticate('jwt')`
+    const { user } = req;
+    // Since we are rendering on the server we have to pass the authenticated user
+    // from `req.user` as `params.user` to our services
+    const params = {
+      user, query: {}
+    };
+    // Find the list of users
+    const users = await app.service('users').find(params);
+    // Find the most recent messages
+    const messages = await app.service('messages').find(params);
+
+    res.render('chat', { user, users, messages });
+  });
+
+  // For the logout route we remove the JWT from the cookie
+  // and redirect back to the login page
+  app.get('/logout', cookieParser(), (req, res) => {
+    res.clearCookie('feathers-jwt');
+    res.redirect('/login');
+  });
 };
